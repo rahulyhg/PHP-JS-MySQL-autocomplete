2458f46cfa57fe8827b10a79a14bc339436a4630
de Christo <dudu.christo@gmail.com>
2017-04-06 09:31:02 -0300

    added date comparisson

--- utils/Date.php
+++ utils/Date.php
@@ -14,6 +14,7 @@ $tomorrow = date('Y-m-d', strtotime($today. ' + 1 days'));
 $yesterday = date('Y-m-d', strtotime($today. ' - 1 days'));
 
 if(strtotime($today) < strtotime($tomorrow)) echo "hoje não é maior que amanhã<br>";
-echo 'y='.strtotime($yesterday)." : ".'t='.strtotime($tomorrow)."<br>";
+//echo 'y='.strtotime($yesterday)." : ".'t='.strtotime($tomorrow)."<br>";
 if(strtotime($yesterday) < strtotime($tomorrow)) echo "ontem é menor que amanhã<br>";
-//
+if((strtotime($tomorrow) > strtotime($yesterday)) &&
+    strtotime($tomorrow) > strtotime($today)) echo "amanhã é maior que hoje e que ontem<br>";
